85af013e37440a5b4e8328bee36c789d9c8d690a
KabusIblis <kabusiblis@gmail.com>
2017-09-20 16:11:09 +0100

    Initial commit
    learning C# to make this bot, hopefully it doesnt go awful.

---
+++ dreambot/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+
+
+
+namespace dreambot
+{
+
+    class Program
+    {
+        public static string token;
+
+        static DiscordClient discord;
+        static CommandsNextModule commands;
+        
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+
+
+        static async Task MainAsync(string[] args)
+        {
+            if (!File.Exists("token.txt"))
+            {
+                Console.WriteLine("Have you set up the token.txt?");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            token = File.ReadAllText("token.txt");
+
+            discord = new DiscordClient(new DiscordConfig
+            {
+                Token = token,
+                TokenType = TokenType.Bot,
+
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug,
+                
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("test"))
+                    await e.Message.RespondAsync("123");
+            };
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "!",
+                EnableDefaultHelp = true
+            });
+
+            commands.RegisterCommands<UserCommands>();
+
+            // private command check,
+            Type privateCheck = Type.GetType("dreambot.PrivateCommands");
+
+            if (privateCheck != null)
+            {
+                Console.WriteLine("PrivateCommand Class  loaded.");
+                commands.RegisterCommands<PrivateCommands>();
+            }
+            else Console.WriteLine("PrivateCommand Class not loaded.");
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
---
+++ dreambot/UserCommands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+
+namespace dreambot
+{
+    public class UserCommands
+
+    {
+        [Command("ass"), Aliases("butt", "bum"), Description("Rates your butt")]
+        public async Task ass(CommandContext ctx)
+        {
+            Random rand = new Random();
+            List<string> buttlist = new List<string>() { "great", "nice", "good", "flat", "pancake", };
+
+            await ctx.RespondAsync($"{buttlist[rand.Next(0, 4)]} ass, {ctx.User.Mention}!");
+        }
+    }
+}
+
